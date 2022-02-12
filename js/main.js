95422f7afda6709915e088fe9df003728256e918
Kateryna Safonova <katrin.safonowa@gmail.com>
2022-02-12 14:24:34 +0200

    suma

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let firstNumber = 5;
+let secondNumber = 10;
+
+function multiply (firstNumber,secondNumber){
+let result = firstNumber + secondNumber;
+ return result;
+}
+console.log( multiply(5, 10) )
